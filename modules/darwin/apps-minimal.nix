998ba7961d73202e58322a1aca9c778f577351e0
Matthew Russell <matthew.russell@narvar.com>
2022-05-20 09:04:20 -0700

    use hydra and remove stack/hls

--- modules/darwin/apps-minimal.nix
+++ modules/darwin/apps-minimal.nix
@@ -11,10 +11,6 @@
     brews = [
       "flyway"
       "postgresql@12"
-      "stack"
-      # Commented out until brew has 1.7.0
-      # Using ghcup for now
-      # "haskell-language-server"
     ];
   };
 }
